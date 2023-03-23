c440b185471d2f2c6097cf6c2db429b2cb1445c0
John Owino <jowino@safaricom.co.ke>
2023-03-23 09:43:21 +0300

    add error control flows for month and year entries

--- jss/scripts.js
+++ jss/scripts.js
@@ -5,10 +5,27 @@ var date = 01
 var month = 01
 var year = 1900
 
+// Date error control flow
 if (date > 31) {
   console.log("Enter a valid date between 1 and 31 for Jan, Mar, May,July, Aug, Oct, Dec; and between 1 and 30 for Apr, Jun, Sep and Nov; and for Feb between 1 & 29 for a leap year and 1 & 28 for a non-leap year");
 } else if (date <= 0) {
   console.log("Enter a valid date between 1 and 31 for Jan, Mar, May,July, Aug, Oct, Dec; and between 1 and 30 for Apr, Jun, Sep and Nov; and for Feb between 1 & 29 for a leap year and 1 & 28 for a non-leap year");
 } else {
-  console.log("Proceed and select gender")
+  console.log("Proceed and enter month of birth as MM");
+}
+
+// Month error control flow
+if (month > 12) {
+  console.log("Enter a valid month between 1 and 12");
+} else if (month <= 0) {
+  console.log("Enter a valid month between 1 and 12");
+} else {
+  console.log("Proceed and enter year of birth as YYYY");
+}
+
+// Year error control flow
+if (year < 1900) {
+  console.log("Enter a valid year between 1900 and today");
+} else {
+  console.log("Proceed and select your gender");
 }
