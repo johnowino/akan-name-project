516c64db484d865da6f4b66d92688b617f5d6035
John Owino <jowino@safaricom.co.ke>
2023-03-24 14:55:04 +0300

    changes layout of forms and added script to calculate day of week

--- jss/scripts.js
+++ jss/scripts.js
@@ -1,4 +1,14 @@
 // variables to be used
-var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-var gender = ["Male", "Female"]
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// var gender = ["Male", "Female"] Try if else to resolve this before asking for date
+//day 0 is Sunday
+
+// Get day script. Return the weekday 
+
+const d = new Date("2023-02-22");
+let day = days[d.getDay()];
+console.log(day);
+
